06775a75c6eecdcfcbb6e33bad50a93eea64bb97
Evgenia Kurineva <kurineva85@mail.ru>
2020-09-16 19:59:55 +0300

    Version 6.0. Classes in Js

--- public_html/script.js
+++ public_html/script.js
@@ -361,11 +361,57 @@ setTimeout(()=>addStylesTo(head2,'blue', '50px'),1500)
 //})
 
 head.addEventListener('click', (event)=> {
-    console.log(event)//mouseEvent with all 
+    console.log(event)//mouseEvent with all
     console.log(event.target.getAttribute('class'))//hello1
      if( head.style.color === 'red'){
         head.style.color = 'green'
     } else {
         head.style.color = 'red'
     }
+})
+
+//CLASS in JS
+class Component {
+    constructor(selector){
+        this.$el=document.querySelector(selector)
+    }
+    hide(){
+        this.$el.style.display='none'
+    }
+    show(){
+        this.$el.style.display='block'
+    }
+}
+
+class Box extends Component{
+    constructor (options){
+        super(options.selector)
+        this.$el.style.width=this.$el.style.height=options.size+'px'
+        this.$el.style.background=options.color
+    }
+}
+
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    color: 'red'
+})
+//box1.hide()
+const box2 = new Box({
+    selector: '#box2',
+    size: 150,
+    color: 'blue'
+})
+
+class Circle extends Box{
+      constructor (options){
+        super(options)
+        this.$el.style.borderRadius='50%'
+    }
+}
+
+const circle1 = new Circle({
+    selector: '#circle1',
+    size: 50,
+    color: 'blue'
 })
